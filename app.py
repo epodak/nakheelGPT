3331d6566abbc6776eab276f4f56eaec36cb995a
yedhu-artefact <yedhu.krishna@artefact.com>
2023-03-10 08:32:20 +0400

    UI Changes and Authentication

--- app.py
+++ app.py
@@ -16,21 +16,6 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.callbacks import get_openai_callback
 import wikipediaapi
 
-
-wiki_doc = wikipediaapi.Wikipedia(
-    language="en", extract_format=wikipediaapi.ExtractFormat.WIKI
-)
-
-
-# def register_user():
-#     if st.button('New User? Register here'):
-#         try:
-#             if authenticator.register_user('Register user', preauthorization=False):
-#                 st.success('User registered successfully')
-#         except Exception as e:
-#             st.error(e)
-
-
 def encrypt_password():
     with open("config.yaml") as file:
         config = yaml.load(file, Loader=SafeLoader)
@@ -45,84 +30,6 @@ def encrypt_password():
     #     file.write(yaml.dump(config, default_flow_style=False))
 
     return config
-
-
-def wiki_search(topic):
-    page_py = wiki_doc.page(topic)
-    title = page_py.title
-    text = page_py.text
-    title = title.lower()
-    # !!!! IMPORTANT encode and decode to remove non-ascii characters
-    title = title.encode("ascii", "ignore").decode()
-    text = text.encode("ascii", "ignore").decode()
-    if title not in os.listdir("data"):
-        with open(f"data/{title}.txt", "w") as f:
-            f.write(text)
-
-
-def generate_answer():
-    user_input = st.session_state.input
-    docs = vectorstore.similarity_search(user_input, k=20)
-
-    print(len(docs))
-    # PART 2 ADDED: CALLBACK FOR TOKEN USAGE
-    with get_openai_callback() as cb:
-        output = chain.run(
-            input=user_input,
-            vectorstore=vectorstore,
-            context=docs,
-            chat_history=[],
-            question=user_input,
-            QA_PROMPT=QA_PROMPT,
-            CONDENSE_QUESTION_PROMPT=CONDENSE_QUESTION_PROMPT,
-            template=_template,
-        )
-        print(cb.total_tokens)
-
-    st.session_state.past.append(user_input)
-    # print(st.session_state.past)
-    st.session_state.generated.append(output)
-    ##ADDED FOR TESTING
-    if "#" in st.session_state.generated[-1]:
-        st.session_state.generated[-1], st.session_state.topics = (
-            st.session_state.generated[-1].split("#")[0],
-            st.session_state.generated[-1].split("#")[1],
-        )
-
-    with open("topics.txt", "w") as f:
-        for char in st.session_state.topics:
-            if char == "[" or char == "]" or char == "'":
-                continue
-            else:
-                f.write(char)
-    print(st.session_state.generated)
-
-
-def rebuild_index():
-    with st.spinner("Cramming documents... Hold on! This may take a while..."):
-        embed_doc()
-        with open("vectorstore.pkl", "rb") as f:
-            vectorstore = pickle.load(f)
-            print("Loading vectorstore...")
-        chain = get_chain(vectorstore)
-
-
-
-
-print("0**************************************")
-
-# From here down is all the StreamLit UI.
-im_icon = Image.open("content/nakheel_icon.png")
-st.set_page_config(page_title="NakheelGPT", page_icon=im_icon)
-
-hide_default_format = """
-       <style>
-       #MainMenu {visibility: hidden; }
-       footer {visibility: hidden;}
-       </style>
-       """
-st.markdown(hide_default_format, unsafe_allow_html=True)
-print("1**************************************")
 
 
 ####
@@ -143,6 +50,101 @@ print("3**************************************")
 
 
 if authentication_status:
+    wiki_doc = wikipediaapi.Wikipedia(
+        language="en", extract_format=wikipediaapi.ExtractFormat.WIKI
+    )
+
+
+    # def register_user():
+    #     if st.button('New User? Register here'):
+    #         try:
+    #             if authenticator.register_user('Register user', preauthorization=False):
+    #                 st.success('User registered successfully')
+    #         except Exception as e:
+    #             st.error(e)
+
+
+    def wiki_search(topic):
+        page_py = wiki_doc.page(topic)
+        title = page_py.title
+        text = page_py.text
+        title = title.lower()
+        # !!!! IMPORTANT encode and decode to remove non-ascii characters
+        title = title.encode("ascii", "ignore").decode()
+        text = text.encode("ascii", "ignore").decode()
+        if title not in os.listdir("data"):
+            with open(f"data/{title}.txt", "w") as f:
+                f.write(text)
+
+
+    def generate_answer():
+        user_input = st.session_state.input
+        docs = vectorstore.similarity_search(user_input, k=20)
+
+        print(len(docs))
+        # PART 2 ADDED: CALLBACK FOR TOKEN USAGE
+        with get_openai_callback() as cb:
+            output = chain.run(
+                input=user_input,
+                vectorstore=vectorstore,
+                context=docs,
+                chat_history=[],
+                question=user_input,
+                QA_PROMPT=QA_PROMPT,
+                CONDENSE_QUESTION_PROMPT=CONDENSE_QUESTION_PROMPT,
+                template=_template,
+            )
+            print(cb.total_tokens)
+
+        st.session_state.past.append(user_input)
+        # print(st.session_state.past)
+        st.session_state.generated.append(output)
+        ##ADDED FOR TESTING
+        if "#" in st.session_state.generated[-1]:
+            st.session_state.generated[-1], st.session_state.topics = (
+                st.session_state.generated[-1].split("#")[0],
+                st.session_state.generated[-1].split("#")[1],
+            )
+
+        with open("topics.txt", "w") as f:
+            for char in st.session_state.topics:
+                if char == "[" or char == "]" or char == "'":
+                    continue
+                else:
+                    f.write(char)
+        print(st.session_state.generated)
+
+
+    def rebuild_index():
+        with st.spinner("Cramming documents... Hold on! This may take a while..."):
+            embed_doc()
+            with open("vectorstore.pkl", "rb") as f:
+                vectorstore = pickle.load(f)
+                print("Loading vectorstore...")
+            chain = get_chain(vectorstore)
+
+
+
+
+    print("0**************************************")
+
+    # From here down is all the StreamLit UI.
+    im_icon = Image.open("content/nakheel_icon.png")
+    st.set_page_config(page_title="NakheelGPT", page_icon=im_icon)
+
+    hide_default_format = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_default_format, unsafe_allow_html=True)
+    print("1**************************************")
+
+
+
+
+
     vectorstore = Chroma(persist_directory="db/", embedding_function=OpenAIEmbeddings())
     print("Loaded vectorstore...")
     chain = get_chain(vectorstore)
