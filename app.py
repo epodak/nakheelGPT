7a6beaad20228aaf4aebb2def52e265bbf7d829b
yedhu-artefact <yedhu.krishna@artefact.com>
2023-03-10 09:24:26 +0400

    UI Changes and Authentication

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from langchain.callbacks import get_openai_callback
 import wikipediaapi
 
 wiki_wiki = wikipediaapi.Wikipedia(
-    language="en", extract_format=wikipediaapi.ExtractFormat.WIKI
+        language='en',
+        extract_format=wikipediaapi.ExtractFormat.WIKI
 )
 
 
@@ -22,7 +23,7 @@ def wiki_search(topic):
     title = page_py.title
     text = page_py.text
     title = title.lower()
-    # !!!! IMPORTANT encode and decode to remove non-ascii characters
+    # !!!! IMPORTANT encode and decode to remove non-ascii characters 
     title = title.encode("ascii", "ignore").decode()
     text = text.encode("ascii", "ignore").decode()
     if title not in os.listdir("data"):
@@ -37,28 +38,17 @@ def generate_answer():
     print(len(docs))
     # PART 2 ADDED: CALLBACK FOR TOKEN USAGE
     with get_openai_callback() as cb:
-        output = chain.run(
-            input=user_input,
-            vectorstore=vectorstore,
-            context=docs,
-            chat_history=[],
-            question=user_input,
-            QA_PROMPT=QA_PROMPT,
-            CONDENSE_QUESTION_PROMPT=CONDENSE_QUESTION_PROMPT,
-            template=_template,
-        )
+        output = chain.run(input=user_input, vectorstore = vectorstore, context=docs, chat_history = [], question= user_input, QA_PROMPT=QA_PROMPT, CONDENSE_QUESTION_PROMPT=CONDENSE_QUESTION_PROMPT, template=_template)
         print(cb.total_tokens)
+    
 
     st.session_state.past.append(user_input)
     # print(st.session_state.past)
     st.session_state.generated.append(output)
     ##ADDED FOR TESTING
     if "#" in st.session_state.generated[-1]:
-        st.session_state.generated[-1], st.session_state.topics = (
-            st.session_state.generated[-1].split("#")[0],
-            st.session_state.generated[-1].split("#")[1],
-        )
-
+        st.session_state.generated[-1], st.session_state.topics = st.session_state.generated[-1].split("#")[0], st.session_state.generated[-1].split("#")[1]
+    
     with open("topics.txt", "w") as f:
         for char in st.session_state.topics:
             if char == "[" or char == "]" or char == "'":
@@ -69,7 +59,7 @@ def generate_answer():
 
 
 def rebuild_index():
-    with st.spinner("Cramming documents... Hold on! This may take a while..."):
+    with st.spinner('Cramming documents... Hold on! This may take a while...'):
         embed_doc()
         with open("vectorstore.pkl", "rb") as f:
             vectorstore = pickle.load(f)
@@ -77,13 +67,14 @@ def rebuild_index():
         chain = get_chain(vectorstore)
 
 
-vectorstore = Chroma(persist_directory="db/", embedding_function=OpenAIEmbeddings())
+vectorstore = Chroma(persist_directory="db/", embedding_function=OpenAIEmbeddings())        
 print("Loaded vectorstore...")
 chain = get_chain(vectorstore)
 
 
+
 # From here down is all the StreamLit UI.
-im_icon = Image.open("content/nakheel_icon.png")
+im_icon = Image.open('content/nakheel_icon.png')
 st.set_page_config(page_title="NakheelGPT", page_icon=im_icon)
 
 hide_default_format = """
@@ -95,23 +86,10 @@ hide_default_format = """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
 st.sidebar.title("NakheelGPT Demo")
-st.sidebar.caption(
-    "Next-Gen ChatBot built on top of the state of the art AI model - ChatGPT."
-)
+st.sidebar.caption("Next-Gen ChatBot built on top of the state of the art AI model - ChatGPT.")
 
 with st.sidebar.expander("Upload a document you would like to chat about! 🚀"):
-    uploaded_file = st.file_uploader(
-        "Upload",
-        type=None,
-        accept_multiple_files=False,
-        key=None,
-        help=None,
-        on_change=None,
-        args=None,
-        kwargs=None,
-        disabled=False,
-        label_visibility="hidden",
-    )
+    uploaded_file = st.file_uploader("Upload",type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="hidden")
 
     # check if file is uploaded and file does not exist in data folder
     if uploaded_file is not None and uploaded_file.name not in os.listdir("data"):
@@ -119,7 +97,7 @@ with st.sidebar.expander("Upload a document you would like to chat about! 🚀")
         with open("data/" + uploaded_file.name, "wb") as f:
             f.write(uploaded_file.getbuffer())
         st.write("File uploaded successfully")
-        with st.spinner("Cramming document..."):
+        with st.spinner('Cramming document...'):
             embed_doc()
 
 if "generated" not in st.session_state:
@@ -129,12 +107,11 @@ if "past" not in st.session_state:
     st.session_state["past"] = []
 
 
-st.text_input("Talk to NakheelGPT: ", value="", key="input", on_change=generate_answer)
+
+st.text_input("Talk to NakheelGPT: ", value="",  key="input", on_change=generate_answer)
 
 st.sidebar.markdown("***")
-st.sidebar.markdown(
-    "Do these topics interest you? Click the button below to add it's wiki articles to my knowledge base 🧠"
-)
+st.sidebar.markdown("Do these topics interest you? Click the button below to add it's wiki articles to my knowledge base 🧠")
 
 # PART 2 ADDED: BUTTONS FOR WIKI ARTICLES
 # buttons need to be in a separate column
@@ -142,7 +119,7 @@ col1, col2, col3 = st.sidebar.columns(3)
 if "topics.txt" in os.listdir("."):
     with open("topics.txt", "r") as f:
         topics = f.read().split(",")
-        if len(topics) >= 3:
+        if len(topics) >= 3: 
             print(topics)
             if col1.button(topics[0]):
                 wiki_search(topics[0])
@@ -155,22 +132,11 @@ if "topics.txt" in os.listdir("."):
                 rebuild_index()
 
 im_logo = Image.open("content/nakheel_logo.png")
-st.sidebar.image(im_logo, use_column_width="auto")
+st.sidebar.image(im_logo, use_column_width='auto')
 
 if st.session_state["generated"]:
 
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-
-        message(
-            st.session_state["generated"][i],
-            key=str(i),
-            avatar_style="bottts",
-            seed="Work",
-        )
-        message(
-            st.session_state["past"][i],
-            is_user=True,
-            key=str(i) + "_user",
-            avatar_style="initials",
-            seed="RD",
-        )
+        
+        message(st.session_state["generated"][i], key=str(i), avatar_style="bottts", seed="Work")
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user", avatar_style="initials", seed="RD")
